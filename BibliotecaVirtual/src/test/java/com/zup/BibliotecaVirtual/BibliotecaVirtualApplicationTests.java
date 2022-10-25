21aaa70d8980f979fe1bfc9fb1584396d8b35d43
Atinhosz Arruda <atos.a.arruda@gmail.com>
2022-10-25 17:10:43 -0300

    primeiro commit

---
+++ BibliotecaVirtual/src/test/java/com/zup/BibliotecaVirtual/BibliotecaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.BibliotecaVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
