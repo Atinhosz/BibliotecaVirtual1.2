eb82b3a6fbbbd5b5d271dde37fd42a9b03bd15f0
Atinhosz Arruda <atos.a.arruda@gmail.com>
2022-10-25 19:12:56 -0300

    Autor e Livro refeito e funcionando

---
+++ BibliotecaVirtual/src/main/java/com/zup/BibliotecaVirtual/modelo/Autor.java
@@ -0,0 +1,54 @@
+package com.zup.BibliotecaVirtual.modelo;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.xml.crypto.Data;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Autor {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    //    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false, length = 400)
+    private String descricao;
+    // @Column(nullable = false) @DateTimeFormat(pattern="yyyy/MM/dd HH:mm:ss")
+    // private LocalDateTime diaDoRegistro = LocalDateTime.now();
+
+    public Autor() {
+    }
+
+    public Autor(String nome, String email, String descricao) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+    }
+
+    public Autor(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    // public LocalDateTime getDiaDoRegistro() {
+    //      return diaDoRegistro;
+    // }
+
+}
---
+++ BibliotecaVirtual/src/main/java/com/zup/BibliotecaVirtual/modelo/Livro.java
@@ -0,0 +1,63 @@
+package com.zup.BibliotecaVirtual.modelo;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+public class Livro {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String titulo;
+    @Column(nullable = false)
+    private String isbn;
+    @Column(nullable = false, length = 400)
+    private String descricao;
+    @Column(nullable = false) @DateTimeFormat(pattern="yyyy/MM/dd HH:mm:ss")
+    private LocalDateTime diaDoRegistro = LocalDateTime.now();
+
+    private Autor autor;
+
+    public Livro(){
+    }
+
+    public Livro(String titulo, String isbn, String descricao, Autor autor) {
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.descricao = descricao;
+        this.autor = autor;
+    }
+
+    public LocalDateTime getDiaDoRegistro() {
+        return diaDoRegistro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Long getID(){
+        return id;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+}
